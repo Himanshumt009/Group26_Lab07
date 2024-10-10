424ad2bffd929a015b9684ae4f6f934478b7c65e
akhilee24mt015 <ee24mt015@iitdh.ac.in>
2024-10-10 23:20:28 +0530

    initializing PORTE

--- main.c
+++ main.c
@@ -13,3 +13,15 @@ void PortF_Initialisation(void) {
     GPIO_PORTF_PUR_R = 0x11;                // Enable pull-up resistor
     GPIO_PORTF_DATA_R = 0x00;               // Reset the data register
 }
+void PORTE_Initialisation(void) {
+    SYSCTL_RCGCGPIO_R |= SYSCTL_RCGC2_GPIOE; // Enable GPIO Port E clock
+    SYSCTL_RCGCUART_R |= (1<<5);             // Enable UART5 clock
+
+    GPIO_PORTE_LOCK_R = GPIO_LOCK_KEY;      // Unlock Port E
+    GPIO_PORTE_CR_R = 0xFF;                 // Commit changes
+    GPIO_PORTE_DEN_R = 0x30;                // Digital enable PE4 and PE5
+    GPIO_PORTE_AFSEL_R = 0x30;              // Enable alternate function
+    GPIO_PORTE_AMSEL_R = 0x00;              // Turn off analog function
+    GPIO_PORTE_PCTL_R &= ~0x00FF0000;
+    GPIO_PORTE_PCTL_R |= 0x00110000;       // Set PE4 and PE5 for UART
+}
